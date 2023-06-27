b7f85e6420c8f43e975f9565ca9b3052c76cad30
KarimGoals++ <abouchar11@gmail.com>
2023-06-27 14:45:47 -0400

    Moved init() method and its helper method, added Javadoc comment

--- src/main/java/com/karimbouchareb/chromatographysimulator/ChromatographySimulatorApp.java
+++ src/main/java/com/karimbouchareb/chromatographysimulator/ChromatographySimulatorApp.java
@@ -107,6 +107,166 @@ public class ChromatographySimulatorApp extends Application {
         launch();
     }
 
+    /**
+     * init() run only once at beginning of application launch -- loads the splash screen with all its ui components
+     */
+    @Override
+    public void init() {
+        // SPLASH SCREEN OBJECTS FOR LOADING
+        splashScreenPane.setBackground(Background.fill(Color.BLACK));
+        // title
+        Pane titlePane = new Pane();
+        ImageView title = makeImageView("title.png");
+        title.setPreserveRatio(true);
+        title.setFitWidth(SCREEN_BOUNDS.getWidth()*0.58);
+        title.setX(SCREEN_BOUNDS.getMinX() + SCREEN_BOUNDS.getWidth() / 2 - title.getFitWidth() / 2);
+        title.setY(SCREEN_BOUNDS.getMinY() + SCREEN_BOUNDS.getHeight() / 2 - title.prefHeight(title.getFitWidth()) / 2);
+        FadeTransition ftTitle = new FadeTransition(Duration.millis(2500), title);
+        ftTitle.setFromValue(0.0);
+        ftTitle.setToValue(0.0);
+        ftTitle.play();
+        ftTitle.setOnFinished(e->{
+            FadeTransition ftTitle2 = new FadeTransition(Duration.millis(1000), title);
+            ftTitle2.setFromValue(0.0);
+            ftTitle2.setToValue(1.0);
+            ftTitle2.play();
+        });
+        titlePane.getChildren().add(title);
+        // background
+        ImageView backgroundScatter = makeImageView("backgroundPopulator.gif");
+        RotateTransition rtBackground = new RotateTransition(Duration.millis(220000), backgroundScatter);
+        rtBackground.setByAngle(360);
+        rtBackground.setAutoReverse(true);
+        rtBackground.setCycleCount(RotateTransition.INDEFINITE);
+        rtBackground.play();
+        backgroundScatter.setFitWidth(SCREEN_BOUNDS.getWidth()*4);
+        backgroundScatter.setFitHeight(SCREEN_BOUNDS.getHeight()*3);
+        // wave
+        Pane wavePane = new Pane();
+        ImageView wave = makeImageView("wave.gif");
+        wave.setRotate(1.8);
+        wave.setFitWidth(title.getFitWidth()*1.15);
+        wave.setFitHeight(title.prefHeight(title.getFitWidth())*0.65);
+        wave.setX(SCREEN_BOUNDS.getMinX() + SCREEN_BOUNDS.getWidth() / 2 - wave.getFitWidth() / 2);
+        wave.setY(SCREEN_BOUNDS.getMinY() + (SCREEN_BOUNDS.getHeight() / 2) + (title.prefHeight(title.getFitWidth())/2) - wave.getFitHeight() / 2);
+        FadeTransition ftWave = new FadeTransition(Duration.millis(2500), wave);
+        ftWave.setFromValue(0.0);
+        ftWave.setToValue(0.0);
+        ftWave.play();
+        ftWave.setOnFinished(e->{
+            FadeTransition ftWave2 = new FadeTransition(Duration.millis(1000), wave);
+            ftWave2.setFromValue(0.0);
+            ftWave2.setToValue(1.0);
+            ftWave2.play();
+        });
+        wavePane.getChildren().add(wave);
+        // Travis
+        Pane travisHeadPane = new Pane();
+        ImageView travisHead = makeImageView("travisHead.png");
+        travisHead.setPreserveRatio(true);
+        travisHead.setFitWidth(SCREEN_BOUNDS.getWidth()*0.1);
+        FadeTransition ftTravisHead1 = new FadeTransition(Duration.millis(5000), travisHead);
+        ftTravisHead1.setFromValue(0.0);
+        ftTravisHead1.setToValue(0.0);
+        ftTravisHead1.play();
+        ftTravisHead1.setOnFinished(e->{
+            FadeTransition ftTravisHead2 = new FadeTransition(Duration.millis(7000), travisHead);
+            ftTravisHead2.setFromValue(0.0);
+            ftTravisHead2.setToValue(1.0);
+            ftTravisHead2.play();
+        });
+        RotateTransition rtTravisHead = new RotateTransition(Duration.millis(10000), travisHead);
+        rtTravisHead.setByAngle(360);
+        rtTravisHead.setCycleCount(RotateTransition.INDEFINITE);
+        rtTravisHead.play();
+        TranslateTransition ttTravisHead = new TranslateTransition(Duration.millis(3000),travisHead);
+        ttTravisHead.setFromX(SCREEN_BOUNDS.getWidth()-Math.random()*SCREEN_BOUNDS.getWidth());
+        ttTravisHead.setFromY(SCREEN_BOUNDS.getHeight()-Math.random()*SCREEN_BOUNDS.getHeight());
+        ttTravisHead.setToX(SCREEN_BOUNDS.getWidth() - Math.random()*SCREEN_BOUNDS.getWidth());
+        ttTravisHead.setToY(SCREEN_BOUNDS.getHeight() - Math.random()*SCREEN_BOUNDS.getHeight());
+        ttTravisHead.play();
+        ttTravisHead.setOnFinished(e->{
+            ttTravisHead.setFromX(travisHead.getTranslateX());
+            ttTravisHead.setFromY(travisHead.getTranslateY());
+            ttTravisHead.setToX(SCREEN_BOUNDS.getWidth() - Math.random()*SCREEN_BOUNDS.getWidth());
+            ttTravisHead.setToY(SCREEN_BOUNDS.getHeight() - Math.random()*SCREEN_BOUNDS.getHeight());
+            ttTravisHead.play();
+        });
+        travisHeadPane.getChildren().add(travisHead);
+        // launchPane
+        Pane launchPane = new Pane();
+        VBox launch = new VBox();
+        launch.setAlignment(Pos.CENTER);
+
+        // launch Button
+        Button launchButton = new Button();
+        launchButton.setBackground(new Background(
+                new BackgroundFill(Color.web("#ffaf00")
+                        ,new CornerRadii(30)
+                        ,Insets.EMPTY)));
+        FontIcon launchIcon = new FontIcon(MaterialDesign.MDI_ARROW_RIGHT_BOLD_HEXAGON_OUTLINE);
+        launchIcon.setIconSize((int) (SCREEN_BOUNDS.getWidth()*0.05));
+        launchIcon.setFill(Color.web("#ff6900"));
+        launchButton.setGraphic(launchIcon);
+        launchButton.setPrefWidth(SCREEN_BOUNDS.getWidth()*0.10);
+        launchButton.setOnMouseEntered(e->{
+            launchButton.setBackground(new Background(
+                    new BackgroundFill(Color.web("#8f1b00")
+                            ,new CornerRadii(30)
+                            ,Insets.EMPTY)));
+        });
+        launchButton.setOnMouseExited(e->{
+            launchButton.setBackground(new Background(
+                    new BackgroundFill(Color.web("#ffaf00")
+                            ,new CornerRadii(30)
+                            ,Insets.EMPTY)));
+        });
+
+        // LAUNCH BUTTON
+        launchButton.setOnAction(e -> {
+            RotateTransition rtPermanent = new RotateTransition(Duration.millis(190), travisHead);
+            rtPermanent.setByAngle(360);
+            rtPermanent.setCycleCount(RotateTransition.INDEFINITE);
+            launchButton.setDisable(true);
+            rtPermanent.play();
+            FadeTransition fadeSplash = new FadeTransition(Duration.seconds(2.2), splashScreenPane);
+            fadeSplash.setFromValue(1.0);
+            fadeSplash.setToValue(0.0);
+            fadeSplash.play();
+            fadeSplash.setOnFinished(actionEvent -> {
+                showMainStage(root); // Leave splash screen and show main stage
+                launchButton.sceneProperty().get().getWindow().hide();
+            });
+        });
+        FadeTransition ftLaunch = new FadeTransition(Duration.millis(2500), launchButton);
+        ftLaunch.setFromValue(0.0);
+        ftLaunch.setToValue(0.0);
+        ftLaunch.play();
+        ftLaunch.setOnFinished(e->{
+            FadeTransition ftLaunch2 = new FadeTransition(Duration.millis(1000), launchButton);
+            ftLaunch2.setFromValue(0.0);
+            ftLaunch2.setToValue(1.0);
+            ftLaunch2.play();
+        });
+        launch.setLayoutX(SCREEN_BOUNDS.getMinX() + (SCREEN_BOUNDS.getWidth()/2) - launchButton.getPrefWidth()/2);
+        launch.setLayoutY(SCREEN_BOUNDS.getMinY() + (SCREEN_BOUNDS.getHeight() / 2) + (title.prefHeight(title.getFitWidth())/2)*1.5 - launch.getHeight() / 2);
+        launch.getChildren().add(launchButton);
+        launchPane.getChildren().add(launch);
+
+        // Place background, travis, wave, title, and launch button into scene
+        splashScreenPane.getChildren().addAll(backgroundScatter,travisHeadPane, wavePane, titlePane, launchPane);
+    }
+
+    // Helper method used inside of init() called when launch button clicked
+    private void showMainStage(Parent root) {
+        mainStage = new Stage(StageStyle.DECORATED);
+        mainStage.setTitle("Gas Chromatography Simulator");
+        mainScene = new Scene(root, SCREEN_BOUNDS.getWidth()*0.98, SCREEN_BOUNDS.getHeight()*0.98);
+        mainStage.setScene(mainScene);
+        mainStage.setMaximized(true);
+        mainStage.show();
+        lineChartSolBand.requestFocus(); // remove focus from clear solute bands button at startup
+    }
 
 // TOP-LEVEL STATIC METHODS
     // INTERNAL CLOCK OF SIMULATION STATIC METHODS
@@ -901,168 +1061,7 @@ public class ChromatographySimulatorApp extends Application {
         }
 
 
-// UI-MANAGEMENT METHODS & INTERFACES
-    public interface InitCompletionHandler {
-        void complete();
-    }
-    // init() run only once at beginning of application launch -- loads splash screen
-    @Override
-    public void init() {
-        // SPLASH SCREEN OBJECTS FOR LOADING
-        splashScreenPane.setBackground(Background.fill(Color.BLACK));
-        // title
-        Pane titlePane = new Pane();
-        ImageView title = makeImageView("title.png");
-        title.setPreserveRatio(true);
-        title.setFitWidth(SCREEN_BOUNDS.getWidth()*0.58);
-        title.setX(SCREEN_BOUNDS.getMinX() + SCREEN_BOUNDS.getWidth() / 2 - title.getFitWidth() / 2);
-        title.setY(SCREEN_BOUNDS.getMinY() + SCREEN_BOUNDS.getHeight() / 2 - title.prefHeight(title.getFitWidth()) / 2);
-        FadeTransition ftTitle = new FadeTransition(Duration.millis(2500), title);
-        ftTitle.setFromValue(0.0);
-        ftTitle.setToValue(0.0);
-        ftTitle.play();
-        ftTitle.setOnFinished(e->{
-            FadeTransition ftTitle2 = new FadeTransition(Duration.millis(1000), title);
-            ftTitle2.setFromValue(0.0);
-            ftTitle2.setToValue(1.0);
-            ftTitle2.play();
-        });
-        titlePane.getChildren().add(title);
-        // background
-        ImageView backgroundScatter = makeImageView("backgroundPopulator.gif");
-        RotateTransition rtBackground = new RotateTransition(Duration.millis(220000), backgroundScatter);
-        rtBackground.setByAngle(360);
-        rtBackground.setAutoReverse(true);
-        rtBackground.setCycleCount(RotateTransition.INDEFINITE);
-        rtBackground.play();
-        backgroundScatter.setFitWidth(SCREEN_BOUNDS.getWidth()*4);
-        backgroundScatter.setFitHeight(SCREEN_BOUNDS.getHeight()*3);
-        // wave
-        Pane wavePane = new Pane();
-        ImageView wave = makeImageView("wave.gif");
-        wave.setRotate(1.8);
-        wave.setFitWidth(title.getFitWidth()*1.15);
-        wave.setFitHeight(title.prefHeight(title.getFitWidth())*0.65);
-        wave.setX(SCREEN_BOUNDS.getMinX() + SCREEN_BOUNDS.getWidth() / 2 - wave.getFitWidth() / 2);
-        wave.setY(SCREEN_BOUNDS.getMinY() + (SCREEN_BOUNDS.getHeight() / 2) + (title.prefHeight(title.getFitWidth())/2) - wave.getFitHeight() / 2);
-        FadeTransition ftWave = new FadeTransition(Duration.millis(2500), wave);
-        ftWave.setFromValue(0.0);
-        ftWave.setToValue(0.0);
-        ftWave.play();
-        ftWave.setOnFinished(e->{
-            FadeTransition ftWave2 = new FadeTransition(Duration.millis(1000), wave);
-            ftWave2.setFromValue(0.0);
-            ftWave2.setToValue(1.0);
-            ftWave2.play();
-        });
-        wavePane.getChildren().add(wave);
-        // Travis
-        Pane travisHeadPane = new Pane();
-        ImageView travisHead = makeImageView("travisHead.png");
-        travisHead.setPreserveRatio(true);
-        travisHead.setFitWidth(SCREEN_BOUNDS.getWidth()*0.1);
-        FadeTransition ftTravisHead1 = new FadeTransition(Duration.millis(5000), travisHead);
-        ftTravisHead1.setFromValue(0.0);
-        ftTravisHead1.setToValue(0.0);
-        ftTravisHead1.play();
-        ftTravisHead1.setOnFinished(e->{
-            FadeTransition ftTravisHead2 = new FadeTransition(Duration.millis(7000), travisHead);
-            ftTravisHead2.setFromValue(0.0);
-            ftTravisHead2.setToValue(1.0);
-            ftTravisHead2.play();
-        });
-        RotateTransition rtTravisHead = new RotateTransition(Duration.millis(10000), travisHead);
-        rtTravisHead.setByAngle(360);
-        rtTravisHead.setCycleCount(RotateTransition.INDEFINITE);
-        rtTravisHead.play();
-        TranslateTransition ttTravisHead = new TranslateTransition(Duration.millis(3000),travisHead);
-        ttTravisHead.setFromX(SCREEN_BOUNDS.getWidth()-Math.random()*SCREEN_BOUNDS.getWidth());
-        ttTravisHead.setFromY(SCREEN_BOUNDS.getHeight()-Math.random()*SCREEN_BOUNDS.getHeight());
-        ttTravisHead.setToX(SCREEN_BOUNDS.getWidth() - Math.random()*SCREEN_BOUNDS.getWidth());
-        ttTravisHead.setToY(SCREEN_BOUNDS.getHeight() - Math.random()*SCREEN_BOUNDS.getHeight());
-        ttTravisHead.play();
-        ttTravisHead.setOnFinished(e->{
-            ttTravisHead.setFromX(travisHead.getTranslateX());
-            ttTravisHead.setFromY(travisHead.getTranslateY());
-            ttTravisHead.setToX(SCREEN_BOUNDS.getWidth() - Math.random()*SCREEN_BOUNDS.getWidth());
-            ttTravisHead.setToY(SCREEN_BOUNDS.getHeight() - Math.random()*SCREEN_BOUNDS.getHeight());
-            ttTravisHead.play();
-        });
-        travisHeadPane.getChildren().add(travisHead);
-        // launchPane
-        Pane launchPane = new Pane();
-        VBox launch = new VBox();
-        launch.setAlignment(Pos.CENTER);
-
-        // launch Button
-        Button launchButton = new Button();
-        launchButton.setBackground(new Background(
-                 new BackgroundFill(Color.web("#ffaf00")
-                ,new CornerRadii(30)
-                ,Insets.EMPTY)));
-        FontIcon launchIcon = new FontIcon(MaterialDesign.MDI_ARROW_RIGHT_BOLD_HEXAGON_OUTLINE);
-        launchIcon.setIconSize((int) (SCREEN_BOUNDS.getWidth()*0.05));
-        launchIcon.setFill(Color.web("#ff6900"));
-        launchButton.setGraphic(launchIcon);
-        launchButton.setPrefWidth(SCREEN_BOUNDS.getWidth()*0.10);
-            launchButton.setOnMouseEntered(e->{
-                launchButton.setBackground(new Background(
-                        new BackgroundFill(Color.web("#8f1b00")
-                                ,new CornerRadii(30)
-                                ,Insets.EMPTY)));
-            });
-            launchButton.setOnMouseExited(e->{
-                launchButton.setBackground(new Background(
-                        new BackgroundFill(Color.web("#ffaf00")
-                                ,new CornerRadii(30)
-                                ,Insets.EMPTY)));
-            });
-
-            // LAUNCH BUTTON
-            launchButton.setOnAction(e -> {
-                RotateTransition rtPermanent = new RotateTransition(Duration.millis(190), travisHead);
-                rtPermanent.setByAngle(360);
-                rtPermanent.setCycleCount(RotateTransition.INDEFINITE);
-                launchButton.setDisable(true);
-                rtPermanent.play();
-                FadeTransition fadeSplash = new FadeTransition(Duration.seconds(2.2), splashScreenPane);
-                fadeSplash.setFromValue(1.0);
-                fadeSplash.setToValue(0.0);
-                fadeSplash.play();
-                fadeSplash.setOnFinished(actionEvent -> {
-                    showMainStage(root); // Leave splash screen and show main stage
-                    launchButton.sceneProperty().get().getWindow().hide();
-                });
-            });
-        FadeTransition ftLaunch = new FadeTransition(Duration.millis(2500), launchButton);
-        ftLaunch.setFromValue(0.0);
-        ftLaunch.setToValue(0.0);
-        ftLaunch.play();
-        ftLaunch.setOnFinished(e->{
-            FadeTransition ftLaunch2 = new FadeTransition(Duration.millis(1000), launchButton);
-            ftLaunch2.setFromValue(0.0);
-            ftLaunch2.setToValue(1.0);
-            ftLaunch2.play();
-        });
-        launch.setLayoutX(SCREEN_BOUNDS.getMinX() + (SCREEN_BOUNDS.getWidth()/2) - launchButton.getPrefWidth()/2);
-        launch.setLayoutY(SCREEN_BOUNDS.getMinY() + (SCREEN_BOUNDS.getHeight() / 2) + (title.prefHeight(title.getFitWidth())/2)*1.5 - launch.getHeight() / 2);
-        launch.getChildren().add(launchButton);
-        launchPane.getChildren().add(launch);
-
-        // Place background, travis, wave, title, and launch button into scene
-        splashScreenPane.getChildren().addAll(backgroundScatter,travisHeadPane, wavePane, titlePane, launchPane);
-    }
-
-    // This method called when launch button clicked
-    private void showMainStage(Parent root) {
-        mainStage = new Stage(StageStyle.DECORATED);
-        mainStage.setTitle("Gas Chromatography Simulator");
-        mainScene = new Scene(root, SCREEN_BOUNDS.getWidth()*0.98, SCREEN_BOUNDS.getHeight()*0.98);
-        mainStage.setScene(mainScene);
-        mainStage.setMaximized(true);
-        mainStage.show();
-        lineChartSolBand.requestFocus(); // remove focus from clear solute bands button at startup
-    }
+// UI-MANAGEMENT METHODS & INTERFACE
 
     // This method called in start() method (which runs after init()) and shows splash screen
     private void showSplash(Stage initStage) {
